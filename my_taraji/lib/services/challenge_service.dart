beca1f11a9f5fa33dceff81033160c2054974dc1
Dhouha BENTRAD <dhouha.bentrad@trendit.fr>
2024-01-03 14:44:10 +0000

    Merged PR 1495: API Next question
    - consomation api next question
    - Merge branch 'Develop' of https://dev.azure.com/trend-it/Queney/_git/MyTaraji.Mobile into feature/coin-challenge
    - commit

--- my_taraji/lib/services/challenge_service.dart
+++ my_taraji/lib/services/challenge_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:my_taraji/core/models/challenge_model.dart';
+import 'package:my_taraji/core/models/next_question_model.dart';
 
 class ChallengeService {
   late String baseUrl = "http://localhost:5074";
@@ -18,6 +19,25 @@ class ChallengeService {
       return challenges;
     } catch (e) {
       throw Exception('Failed to connect to the server for challenge data $e');
+    }
+  }
+
+  Future<NextQuestion> getNextQuestionByStepId(String stepid) async {
+    const path = "api/v1/challenges/next-question";
+    final url = Uri.parse('$baseUrl/$path/$stepid');
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> jsonData = json.decode(response.body);
+        NextQuestion nextQuestion = NextQuestion.fromJson(jsonData);
+        return nextQuestion;
+      } else {
+        throw Exception('Failed to load next question data');
+      }
+    } catch (e) {
+      throw Exception(
+          'Failed to connect to the server for next question data by step id $e');
     }
   }
 }
